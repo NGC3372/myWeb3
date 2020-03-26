71ea607384e9f008f7326c2cf7cc289f27fe111a
NGC3372 <49350058+NGC3372@users.noreply.github.com>
2020-03-26 12:05:06 +0800

    Update myWeb3.py
    re-design some function

--- myWeb3.py
+++ myWeb3.py
@@ -1,6 +1,5 @@
 from  web3 import Web3 ,HTTPProvider
 
-
 class myWeb3:
     #对象初始化
     def __init__(self , web3Provider = None , chainID = None):
@@ -12,79 +11,88 @@ class myWeb3:
         self.ETH = self.w3.eth
         self.defaultGasPrice = self.ETH.gasPrice
         self.initAccount()
-    #初始化账户     
-    def initAccount(self , address = None , privateKey = None):
-        if address is None:
-            self.address = '0x76e0eC050466c4dddBbAA25C3f069C18F026AF95'
-            self.privateKey = 'DDA1EC2A2312151AC7D82CB9842B72C93EDCAD903B44A185387CD53FD2C29625'
+    #初始化账户
+    def initAccount(self , privateKey = None):
+        if privateKey is None:
+            self.account = self.ETH.account.privateKeyToAccount('DDA1EC2A2312151AC7D82CB9842B72C93EDCAD903B44A185387CD53FD2C29625')
         else:
-            self.address = address
-            self.privateKey = privateKey 
-    #发起交易        
-    def makeTransaction(self , to_ : str, value : int, gasPrice = None):
+            self.account = self.ETH.account.privateKeyToAccount(privateKey)
+    #发起交易
+    def makeTransaction(self , to_: str, value: int, gasPrice = None , gasLimit = None):
         if gasPrice is None:
             gasPrice = self.defaultGasPrice
-        nonce = self.ETH.getTransactionCount(self.address)
+        if gasLimit is None:
+            gasLimit = 200000
+        nonce = self.ETH.getTransactionCount(self.account.address)
         transaction = {
             'to': to_,
             'value': value,
-            'gas': 200000,
+            'gas': gasLimit,
             'gasPrice': gasPrice,
-            'chainId': 3,
+            'chainId': self.chainID,
             'nonce': nonce
         }
-        signedTx = self.ETH.account.sign_transaction(transaction , self.privateKey)
+        signedTx = self.account.sign_transaction(transaction)
         hashTx = self.ETH.sendRawTransaction(signedTx.rawTransaction).hex()
         return hashTx
+    #建立合约  web3.contract.ContractConstructor
+    def createContract(self , byteCode:str , ABI:str ,*constructArgs):
+        CONTRACT = self.ETH.contract(abi=ABI, bytecode=byteCode).constructor(constructArgs)
+        return CONTRACT
     #部署合约
-    def deployContract(self , byteCode : str, gasPrice = None):
+    def deployContract(self ,contract , gasPrice = None ,gasLimit = None ):
         if gasPrice is None :
             gasPrice = self.defaultGasPrice
-        nonce = self.ETH.getTransactionCount(self.address)
-        transaction = {
+        if gasLimit is None:
+            gasLimit = 3000000
+        nonce = self.ETH.getTransactionCount(self.account.address)
+        transaction = contract.buildTransaction({
             'nonce':nonce,
             'gasPrice': gasPrice,
-            'gas': 3000000,
-            'data':byteCode
-        }   
-        signedTx = self.ETH.account.signTransaction(transaction, self.privateKey)
+            'gas': gasLimit,
+        })
+        signedTx = self.account.signTransaction(transaction)
         hashTx = self.ETH.sendRawTransaction(signedTx.rawTransaction).hex()
         contractAddress =  self.ETH.waitForTransactionReceipt(hashTx)['contractAddress']
         return {'hashTx' : hashTx , 'contractAddress':contractAddress}
     #获取合内置函数
-    def getContractFunction(self , contractAddress , ABI , functionName : str):
+    def getContractFunction(self , contractAddress:str , ABI:str , functionName : str):
         contract = self.ETH.contract(address=contractAddress ,abi=ABI)
-        function = contract.functions[functionName]
+        function = contract.find_functions_by_name(functionName)[0]
         return function
-    #调用合约内置函数
-    def runContractFunction(self , useGas : bool , function , gasPrice = None):
-        if useGas :
-            if gasPrice is None:
-                gasPrice = self.defaultGasPrice
-            nonce = self.ETH.getTransactionCount(self.address)
-            function_txn = function.buildTransaction({
-                'chainId':3,
-                'gas': 70000,
-                'gasPrice': gasPrice,
-                'nonce': nonce
-            })
-            signed_txn = self.ETH.account.sign_transaction(function_txn, private_key = self.privateKey)
-            hashTx = self.ETH.sendRawTransaction(signed_txn.rawTransaction).hex()
-            return hashTx
-        else:
-            result = function.call()
-            return result
+    #调用合约内置函数transcation
+    def transcationContractFunction(self , function , gasPrice = None , value = None , gasLimit = None):
+        if gasPrice is None:
+            gasPrice = self.defaultGasPrice
+        if value is None:
+            value = 0
+        if gasLimit is None:
+            gasLimit = 200000
+        nonce = self.ETH.getTransactionCount(self.account.address)
+        function_txn = function.buildTransaction({
+            'gas': gasLimit,
+            'gasPrice': gasPrice,
+            'nonce': nonce,
+            'value': value
+        })
+        signed_txn = self.account.sign_transaction(function_txn)
+        hashTx = self.ETH.sendRawTransaction(signed_txn.rawTransaction).hex()
+        return hashTx
+    #调用合约内置函数call  
+    def callContractFunction(self , function):
+        result = function.call()
+        return result
     #获取账户余额
     def getBalance(self , address = None):
         if address is None:
             address = self.address
-        balance = self.ETH.getBalance(address)    
-        return balance 
+        balance = self.ETH.getBalance(address)
+        return balance
     #获取交易收据的详细信息
     def getReceiptInfoFromHash(self , hashTx : str):
         info = self.ETH.getTransactionReceipt(hashTx)
-        return info   
-    #获取交易详细信息     
+        return info
+    #获取交易详细信息
     def getTransactionInfoFromHash(self , hashTx : str):
         info = self.ETH.getTransaction(hashTx)
         return info
